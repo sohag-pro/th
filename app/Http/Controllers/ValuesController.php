86bfd09ac1331046fb746f1b275909f42d90ced5
shuvo575 <shohaghasan575@gmail.com>
2019-12-25 11:45:20 +0600

    Getting all Values and Specific Keys

---
+++ app/Http/Controllers/ValuesController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\Values as ValuesResource;
+use App\Values;
+
+use Illuminate\Http\Request;
+
+class ValuesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        
+        if ($request->has('keys')) {
+            $key = $request->input('keys');
+            $key = explode(',', $key);
+            // dd($key);
+            return new ValuesResource(Values::all()->whereIn('key', $key));
+        }else{
+            return new ValuesResource(Values::all());
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        if ($request->has('keys')) {
+            $key = $request->input('keys');
+        }
+        return new ValuesResource(Values::firstOrFail()->where('key', $key));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/values', 'ValuesController@index');
+Route::get('/values/{key}', 'ValuesController@index');
